6fe5e1239156544e6d2a70f48855b5b1f80b4d64
StefanoDellamore <146744495+StefanoDellamore@users.noreply.github.com>
2024-05-13 14:59:37 +0200

    Create input & select

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -12,10 +12,30 @@ export default {
 </script>
 
 <template>
+
     <div>
-        Converter
+        <label for="amountInput" class="label"></label>
+            <input type="number" id="amountInput">
+
+        <label class="label"></label>
+        <select name="currency" id="currency">
+            <option value="currency">eur</option>
+            <option value="currency">usd</option>
+        </select>
+
+
+        <label for="convertedAmount" class="label"></label>
+            <input type="number" id="convertedAmount">
+
+        <label class="label"></label>
+        <select name="currency" id="currency">
+            <option value="currency">eur</option>
+            <option value="currency">usd</option>
+        </select>
+        
     </div>
+
 </template>
 
 <style lang="scss" scoped>
-</style>
+</style>amount Input
